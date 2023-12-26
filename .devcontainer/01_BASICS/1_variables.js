34e1a619368bfef6294c655b8a02fa6770aa1fe2
Ayushpandey7890 <154805811+Ayushpandey7890@users.noreply.github.com>
2023-12-26 18:45:29 +0000

    conversion

---
+++ .devcontainer/01_BASICS/1_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1445556
+let accountEmail = "PANDEAYUSH9562GMAIL.COM"
+var accountPassword = "123345"
+accountCity = "Jaipur pink city"
+
+// accountId = 2 // not alowed in JS
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+let accountState;
+accountEmail = "hcverma@gmail.com"
+accountCity = "bombay"
+accountPassword = "33334"
+
+console.log(accountId);
+
+console.table([accountCity,accountEmail,accountId,accountPassword,accountState])
