6f0b0f47161818528ae721925b293e5a36e8ce83
Ayushpandey7890 <154805811+Ayushpandey7890@users.noreply.github.com>
2023-12-26 13:51:47 +0000

    datatypes

---
+++ .devcontainer/01_BASICS/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3) // code readability should be high this should not be used although output will be there but good practice in coding
+
+console.log("ayush")
+
+let name = "ayush"
+let age = 30
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol(unique) used in react framework
+// object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object interview question
