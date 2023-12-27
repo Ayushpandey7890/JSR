d42e66e3592fae30a22a18ffb4db794b07a1a0c5
Ayushpandey7890 <154805811+Ayushpandey7890@users.noreply.github.com>
2023-12-27 13:18:28 +0000

    comparisons data type yess

--- .devcontainer/01_BASICS/4_comparisons datatype.js
+++ .devcontainer/01_BASICS/4_comparisons datatype.js
@@ -1,5 +1,20 @@
-console.log(2>2);
-console.log(2>=3);
-console.log(2%5);
-console.log(3==2);
-console.log(2!=4);
+// console.log(2>2);
+// console.log(2>=3);
+// console.log(2%5);
+// console.log(3==2);
+// console.log(2!=4);
+
+// console.log("2 "> 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);  // interesting results
+
+console.log(undefined ==0);
+console.log(undefined > 0); // all these codes are usually avoided 
+console.log(undefined < 0);
+
+// strict check === value ke saath saath uske datatype ko bhi check krta hai//
+
+console.log("2" ===2);
