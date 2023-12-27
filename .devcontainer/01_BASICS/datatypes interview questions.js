f1958f949e32113be2ed44f5bd2b4f031443e6f4
Ayushpandey7890 <154805811+Ayushpandey7890@users.noreply.github.com>
2023-12-27 14:29:34 +0000

    datatypes interview questions

---
+++ .devcontainer/01_BASICS/datatypes interview questions.js
@@ -0,0 +1,29 @@
+// 2 types of data types PRIMITIVE AND NON PRIMITIVE
+
+// 7 TYPES : STRING, NUMBER, BOOLEAN, NULL, UNDEFINED, SYMBOL (TO MAKE ANY VALUE UNIQUE) , BIGINT(BIG VALUES)
+
+const score = 100
+const scoreValue = 100.3
+
+const issLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 444433444355354545333335553n
+
+// REFERENCE TYPE (NON PRIMITIVE)
+
+// ARRAYS, OBJECTS, FUNCTIONS
+
+// JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+const heroes = ("shaktimaan","naagaraj","dojacat")
+let myObj = {
+    name: "ayush"
+    age:22,
+}
